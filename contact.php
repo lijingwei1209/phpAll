61e467a7089d166e13aee312ed28cf9f5c228654
lijingwei1209 <1004451027@qq.com>
2016-07-10 23:14:57 +0800

    tel

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@ include ('qq.php');
                     <dd class="margin_tb20"><h2>公司联系方式</h2></dd>
                     <dd class="margin_tb10">地址：郑州市高新区长椿路11号国家大学科技园</dd>
                     <dd class="margin_tb10">联系人：王经理</dd>
-                    <dd class="margin_tb10">电话：17739798657  / 18903835209</dd>
+                    <dd class="margin_tb10">电话：<a href="tel:17739798657">17739798657</a>  / <a href="tel:18903835209">18903835209</a></dd>
                     <dd class="margin_tb10">邮箱：<a href="mailto:wangzhihui1369@163.com" style="text-decoration: none">wangzhihui1369@163.com</a></dd>
                     <dd class="margin_tb10">网址：<a href="http://www.tengyubio.com">www.tengyubio.com</a> </dd>
                 </dl>
